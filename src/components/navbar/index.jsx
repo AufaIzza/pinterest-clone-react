83c72d0c787ae02a626f764d107ef84143c8c56f
AufaIzza <aufashiqiizza@gmail.com>
2024-04-23 12:41:47 +0700

    adding navbar

---
+++ src/components/navbar/index.jsx
@@ -0,0 +1,37 @@
+import { Link } from "react-router-dom"
+
+const index = () => {
+  return (
+    <header>
+      <div class="navbar-left">
+        <a class="nav-icon" href="logo"><i class='bx bxl-pinterest'></i></a>
+
+        <div class="navbar">
+          <ul class="navlist-left">
+            <li class="nav-item"><Link to="/today">Today</Link></li>
+            <li class="nav-item"><Link to="/watch">Watch</Link></li>
+            <li class="nav-item"><Link to="/explore">Explore</Link></li>
+          </ul>
+        </div>
+
+        <div class="navbar-right">
+          <ul class="navlist-right">
+            <li class="nav-item"><Link to="Tentang">About</Link></li>
+            <li class="nav-item"><Link to="Bisnis">Business</Link></li>
+            <li class="nav-item"><Link to="Bisnis">Blog</Link></li>
+          </ul>
+
+          <div class="button">
+            <button class="login">Log in</button>
+            <button>Sign up</button>
+          </div>
+        </div>
+        <div class="menu-toggle">
+          <i class='bx bx-menu'></i>
+        </div>
+      </div>
+    </header>
+  )
+}
+
+export default index
