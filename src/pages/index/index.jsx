7e978a05637016a3ca21015e85b9c624c5f637a3
AufaIzza <aufashiqiizza@gmail.com>
2024-04-23 14:37:27 +0700

    adding section 1

--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -1,9 +1,13 @@
 import NavBar from "../../components/navbar/index"
+import Hero from "./components/hero/index"
+import Section1 from "./components/section-1/index"
 
 const index = () => {
   return (
     <div>
       <NavBar/>
+      <Hero/>
+      <Section1/>
     </div>
   )
 }
