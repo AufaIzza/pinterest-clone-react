8c38fb37429d5a6b0ffa1583cb788b3e00f58168
AufaIzza <aufashiqiizza@gmail.com>
2024-04-23 12:11:56 +0700

    adding index pages to router

---
+++ src/router/index.jsx
@@ -0,0 +1,19 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Index from "../pages/index/index"
+
+const index = () => {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: (
+	<Index/>
+      )
+    }
+  ])
+
+  return (
+    <RouterProvider router={router}/>
+  )
+}
+
+export default index
