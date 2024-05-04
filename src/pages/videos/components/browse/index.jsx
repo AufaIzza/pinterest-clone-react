010c0953b43e2e4ffd86f4c7fe71bf0ed7232a5a
jordie.juwono <jordie.juwono@indocyber.id>
2024-05-04 20:03:33 +0700

    add category for art to browseList

--- src/pages/videos/components/browse/index.jsx
+++ src/pages/videos/components/browse/index.jsx
@@ -59,6 +59,35 @@ const browseList = [
                 backgroundImage: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRan4LQL0LMDggfkuefPxkp_gkowYuLuVF02MFuiX0ioQ&s",
             },
         ]
+    },
+    {
+        categoryName: "Art",
+        data: [
+            {
+                duration: "0:23",
+                backgroundImage: "https://www.pictoclub.com/wp-content/uploads/2021/09/painting-brushes-scaled.jpg",
+            },
+            {
+                duration: "0:46",
+                backgroundImage: "https://c4.wallpaperflare.com/wallpaper/283/881/127/the-great-wave-off-kanagawa-painting-japanese-waves-wallpaper-preview.jpg",
+            },
+            {
+                duration: "1:20",
+                backgroundImage: "https://cdn11.bigcommerce.com/s-x49po/images/stencil/1500x1500/products/82780/254882/1644476543451_InShot_20220210_114402995__06847.1687161220.jpg?c=2",
+            },
+            {
+                duration: "0:45",
+                backgroundImage: "https://media.artsper.com/homepage/mainImageThematic.jpg",
+            },
+            {
+                duration: "2:30",
+                backgroundImage: "https://onecms-res.cloudinary.com/image/upload/s--TQ6GS892--/f_auto,q_auto/c_fill,g_auto,h_676,w_1200/v1/mediacorp/cna/image/2023/02/27/istock-1357759108.jpg?itok=ozNsprgA",
+            },
+            {
+                duration: "0:33",
+                backgroundImage: "https://i.pinimg.com/736x/29/7f/46/297f46721b637a38e3567e1663e2aa80.jpg",
+            },
+        ]
     }
 ];
 
