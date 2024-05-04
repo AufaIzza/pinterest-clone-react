47d19451d164037e259538dc1945a91ff1b0b3df
AufaIzza <aufashidqiizza@gmail.com>
2024-05-04 21:00:48 +0700

    fixed up leaking css from ideas

---
+++ src/pages/today/components/section-2/index.jsx
@@ -0,0 +1,14 @@
+import Image1 from "../../assets/3ef1746ee5289eb936a5c4d8479ecf7c.jpg"
+import Image2 from "../../assets/Screenshot_20240504_204829.png"
+import Image3 from "../../assets/Screenshot_20240504_204420.png"
+import Image4 from "../../assets/4fc75a6de49231c737484e6ef6e9b944.jpg"
+
+const index = () => {
+    return(
+        <div>
+            <img src={Image1} alt="" />
+        </div>
+    )
+}
+
+export default index
---
+++ src/pages/today/index.jsx
@@ -0,0 +1,11 @@
+import Section2 from "./components/section-2/index"
+
+const index = () => {
+    return(
+        <div>
+            <Section2/>
+        </div>
+    )
+}
+
+export default index
