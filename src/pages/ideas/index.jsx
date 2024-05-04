15ae970a6f901a89a2856d02c6759d3c7a1cf731
ridwanrahmatn <ridwanrahmtn@gmail.com>
2024-05-04 20:23:22 +0700

    tambah section 3

--- src/pages/ideas/index.jsx
+++ src/pages/ideas/index.jsx
@@ -1,6 +1,7 @@
 import NavBar from '../../components/navbar/index'
 import Section1 from './components/section1/index'
 import Section2 from './components/section2/index'
+import Section3 from './components/section3/index'
 import './style.css'
 
 const index = () => {
@@ -8,6 +9,7 @@ const index = () => {
         <NavBar/>
         <Section1/>
         <Section2/>
+        <Section3/>
     </div>)
 }
 
