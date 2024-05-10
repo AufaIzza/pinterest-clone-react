24007fca6ee5296d8e3cba754427c37c3a6c132d
FelixStevian11 <felixstevian00@gmail.com>
2024-05-10 19:07:54 +0800

    update navbar to tailwind

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,33 +1,28 @@
 import { Link } from "react-router-dom"
-import "./styles.css"
+
 
 const index = () => {
   return (
-    <header>
-      <div className="navbar-left">
-        <Link className="nav-icon" to={"/"}><i className='bx bxl-pinterest'></i></Link>
+    <header className="flex px-[2%] py-px fixed w-full bg-[white] shadow-xl z-[100]">
+      <div className="mr-auto flex justify-center items-center">
+        <Link className="cursor-pointer mr-auto text-[red] text-[60px]" to={"/"}><i className='bx bxl-pinterest'></i></Link>
 
         <div className="navbar">
-          <ul className="navlist-left">
-            <li className="nav-item"><Link to="/today">Today</Link></li>
-            <li className="nav-item"><Link to="/watch">Watch</Link></li>
-            <li className="nav-item"><Link to="/ideas">Explore</Link></li>
+          <ul className="flex items-center no-underline list-none">
+            <div className="flex gap-[40px] px-[40px] py-0">
+            <li className="after:content-[''] after:h-[2px] after:w-[0] after:bg-[black] after:block after:opacity-0 after:[transition:.4s] hover:after:opacity-100 hover:after:w-full"><Link to="/today">Today</Link></li>
+            <li className="after:content-[''] after:h-[2px] after:w-[0] after:bg-[black] after:block after:opacity-0 after:[transition:.4s] hover:after:opacity-100 hover:after:w-full"><Link to="/watch">Watch</Link></li>
+            </div>
           </ul>
         </div>
       </div>
-
-        <div className="navbar-right">
-          <ul className="navlist-right">
-          </ul>
-
-          <div className="button">
-            <button className="login">Log in</button>
-            <button>Sign up</button>
+          <div className=" flex justify-center items-center">
+            <ul>
+              <li className="mr-[40px] after:content-[''] after:h-[2px] after:w-[0] after:bg-[black] after:block after:opacity-0 after:[transition:.4s] hover:after:opacity-100 hover:after:w-full"><Link to="/ideas">Explore</Link></li>
+            </ul>
+            <button className="px-[25px] py-[9px] bg-[red] text-[white] border-[none] rounded-[50px] cursor-pointer hover:bg-[rgb(192,_2,_2)]">Log in</button>
+            <button className="px-[25px] py-[9px] bg-[#D8D5D4] border-[none] rounded-[50px] cursor-pointer ml-[10px] hover:bg-[#b4b3b3]">Sign up</button>
           </div>
-        </div>
-        <div className="menu-toggle">
-          <i className='bx bx-menu'></i>
-        </div>
     </header>
   )
 }
