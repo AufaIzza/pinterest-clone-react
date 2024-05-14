f39c53d0eb9750ee704c825d2e7b7204094f521b
wahyunup <wahyunurpambuko@gmail.com>
2024-05-14 10:34:28 +0700

    revision section3

--- src/pages/index/components/section-3/index.jsx
+++ src/pages/index/components/section-3/index.jsx
@@ -1,28 +1,19 @@
 import { Link } from "react-router-dom"
 import ImageSection3 from "./assets/imageSection3.png"
-import "./styles.css"
 
 const index = () => {
     return (
-        <section id="section3">
-            <div className="containersection3">
-                <div>
-                    <img src={ImageSection3} alt=""/>
-                </div>
-                <div>
-                    <div className="wrapper">
-                        <div className="wrap">
-                            <div>
-                                <h1>See it, make it, try it, do it</h1>
-                                <p>The best part of Pinterest is discovering new things and ideas from people around the world.</p>
-                            </div>
-                            <div className="eksplore">
-                                <Link to={"/ideas"}>Explore</Link>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+        <section id="section3" >
+           <div className="flex items-center bg-[#FFE2EB] flex-wrap">
+            <div className="image sm:w-full md:w-full  lg:w-1/2">
+                <img src={ImageSection3} alt="" />
             </div>
+            <div className="lg:text text-center w-[450px] px-[32px] mx-auto text-[#C32F00] sm:py-[100px]">
+                <h1 className="text-[60px] font-bold pb-[21px]">See it, make it, try it, do it</h1>
+                <p className="text-[23.81px] pb-[21px]">The best part of Pinterest is discovering new things and ideas from people around the world.</p>
+                <a className="text-white rounded-full text-[16px] font-semibold px-6 py-3 bg-[#E60023]" href="#">explore</a>
+            </div>
+           </div>
           </section>
     )
 }
--- src/pages/today/index.jsx
+++ src/pages/today/index.jsx
@@ -4,18 +4,11 @@ import Image1 from '../../pages/today/assets/1.jpg'
 
 const index = () =>{
     return(
-<<<<<<< HEAD
         <>
         <NavBar/>
         <Card/>
         
         </>
-=======
-        <div>
-            <NavBar/>
-            <Section2/>
-        </div>
->>>>>>> 8500203ff6b50fabf809ff1b636c68e05b3be845
     )
 }
 export default index;
